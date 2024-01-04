c441d1a84f799fe0a6e92187508f10bf16a6a738
Pavel <you@example.com>
2024-01-04 05:17:49 +0300

    feat: implement isPalindrome task (04.01.24 05:17)

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -247,8 +247,17 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let str1 = '';
+  let str2 = '';
+  for (let i = 0; i < Math.floor(str.length / 2); i += 1) {
+    str1 += str[i];
+    str2 += str[str.length - i - 1];
+  }
+  if (str1 === str2) {
+    return true;
+  }
+  return false;
 }
 
 /**
